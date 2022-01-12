c4c6c1cc131b8560bf87cbaa2218815141861f8c
Bijay Bohora <bijayb37@gmail.com>
2022-01-13 02:07:56 +1100

    SCRIPT NEXT ISSUES

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,14 @@ import Projects from '../Components/Projects'
 import Navbar from '../Components/Navbar'
 import styles from '../styles/Index.module.scss'
 import Head from 'next/head'
+import Script from 'next/script'
 
 export default function Index() {
   return (
     <div className={styles.app}>
       <Head>
       <title>Bijay Bohora</title>
-      <script src="https://kit.fontawesome.com/973ad33301.js" crossOrigin="anonymous"></script>
+      <Script src="https://kit.fontawesome.com/973ad33301.js" crossOrigin="anonymous"></Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet" />
