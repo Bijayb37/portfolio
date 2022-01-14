4f7dd3580d2d2322aa0145872c9d69e031cffd20
Bijay Bohora <bijayb37@gmail.com>
2022-01-14 23:31:17 +1100

    colors

--- Components/data.js
+++ Components/data.js
@@ -4,13 +4,20 @@ export const projectData = [
     demoUrl: "https://bb-issuetracker.netlify.app",
     imageUrl: "/issueTracker.png",
     role: ["full stack developer"],
-    tech: ['react', 'redux', 'node']
+    tech: ['react', 'redux', 'html5', 'css3']
   },
   {
     title: "Twit Social Media App",
     demoUrl: "https://bb-twit.netlify.app",
     imageUrl: "/twit.png",
     role: ["full stack developer"],
-    tech: ['react', 'redux', 'node']
+    tech: ['react', 'redux', 'html5', 'css3']
+  },
+  {
+    title: "Color Pallete",
+    demoUrl: "https://color-pallete-with-hooks.vercel.app",
+    imageUrl: "/colors.png",
+    role: ["full stack developer"],
+    tech: ['react', 'html5', 'css3']
   },
 ]
